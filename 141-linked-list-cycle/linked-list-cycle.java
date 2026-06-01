8217d2e461ec43df6c443d070d14d2b37bb1e86f
GOWTHAM N <nakkagowtham399@gmail.com>
2026-06-01 17:29:22 +0530

    Time: 0 ms (100.00%) | Memory: 46.9 MB (32.11%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.java
+++ 141-linked-list-cycle/linked-list-cycle.java
@@ -11,19 +11,15 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        HashSet<ListNode>map=new HashSet<>();
-        ListNode temp=head;
-        if(head==null||head.next==null){
-            return false;
+       ListNode s=head;
+       ListNode f=head;
+       while(f!=null&&f.next!=null){
+        s=s.next;
+        f=f.next.next;
+        if(s==f){
+            return true;
         }
-        while(head!=null&&head.next!=null){
-            if(map.contains(head)){
-                return true;
-            }
-            map.add(head);
-              head=head.next;
-            
-        }
+       }
         return false;
     }
 }
