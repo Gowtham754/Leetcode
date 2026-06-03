ff32fc908ecf1100395ff1b31429720d50faa5b7
GOWTHAM N <nakkagowtham399@gmail.com>
2026-06-03 15:54:46 +0530

    Time: 5 ms (33.91%) | Memory: 46.8 MB (20.66%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int romanToInt(String s) {
+       HashMap<Character,Integer> map=new HashMap<>();
+       map.put('I',1); 
+       map.put('V',5); 
+       map.put('X',10); 
+       map.put('L',50);
+       map.put('C',100);
+       map.put('D',500);
+       map.put('M',1000);
+       int res=0;
+       for(int i=0;i<s.length()-1;i++){
+        char ch=s.charAt(i);
+        char ch1=s.charAt(i+1);
+        if(map.get(ch)<map.get(ch1)){
+            res-=map.get(ch);
+        }
+        else{
+            res+=map.get(ch);
+        }
+       }
+       return res+map.get(s.charAt(s.length()-1)); 
+    }
+}
