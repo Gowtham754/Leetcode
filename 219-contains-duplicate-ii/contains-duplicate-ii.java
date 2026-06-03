686b9a53312b333d0ebb4d82f7f2cbbaf72d0e80
GOWTHAM N <nakkagowtham399@gmail.com>
2026-06-03 11:17:53 +0530

    Time: 22 ms (90.45%) | Memory: 90.7 MB (24.86%) - LeetSync

---
+++ 219-contains-duplicate-ii/contains-duplicate-ii.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+       HashMap<Integer,Integer> map=new HashMap<>();
+       int i=0;
+       while(i<nums.length){
+        if(map.containsKey(nums[i])){
+            if(i-map.get(nums[i])<=k){
+                return true;
+            }
+        }
+        map.put(nums[i],i);
+        i++;
+       }
+       return false;
+    }
+}
