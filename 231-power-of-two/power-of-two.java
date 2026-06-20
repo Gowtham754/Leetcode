dcd95f9723d5156fa0291d2d261d5325df48dda7
GOWTHAM N <nakkagowtham399@gmail.com>
2026-06-20 16:19:37 +0530

    Time: 1 ms (96.46%) | Memory: 42.4 MB (71.96%) - LeetSync

--- 231-power-of-two/power-of-two.java
+++ 231-power-of-two/power-of-two.java
@@ -1,11 +1,6 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        int cnt=0;
-      while(n>0){
-         if((n&1)!=0) cnt++;
-         n>>=1;
-      }
-      if(cnt==1) return true;
-      else return false; 
+        if(n<=0) return false;
+      return (n&(n-1))==0; 
     }
 }
