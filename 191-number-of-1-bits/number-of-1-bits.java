b9efed9ecf3b5cef08975f8a5462259961f2b24b
GOWTHAM N <nakkagowtham399@gmail.com>
2026-06-21 17:21:46 +0530

    Time: 1 ms (30.76%) | Memory: 42.1 MB (91.07%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.java
+++ 191-number-of-1-bits/number-of-1-bits.java
@@ -1,11 +1,9 @@
 class Solution {
     public int hammingWeight(int n) {
         int cnt=0;
-       String b=String.format("%32s",Integer.toBinaryString(n).replace(' ','0'));
-       for(char ch:b.toCharArray()){
-        if(ch=='1'){
-            cnt++;
-        }
+       while(n!=0){
+        cnt+=(n&1);
+        n>>>=1;
        }
 
     return cnt;
