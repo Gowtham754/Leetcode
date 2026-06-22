0c8e76581e8a72cdc5d4c51c219c2e5708176091
GOWTHAM N <nakkagowtham399@gmail.com>
2026-06-22 06:29:33 +0530

    Time: 2 ms (96.67%) | Memory: 43.3 MB (79.23%) - LeetSync

--- 1297-maximum-number-of-balloons/maximum-number-of-balloons.java
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.java
@@ -1,14 +1,17 @@
 class Solution {
     public int maxNumberOfBalloons(String text) {
-       HashMap<Character,Integer>map=new HashMap<>();
-       for(char ch:text.toCharArray()){
-        map.put(ch,map.getOrDefault(ch,0)+1);
-       }
-       int b=map.getOrDefault('b',0);
-       int a=map.getOrDefault('a',0);
-       int l=map.getOrDefault('l',0)/2;
-       int o=map.getOrDefault('o',0)/2;
-       int n=map.getOrDefault('n',0);
-       return Math.min(Math.min(b,a),Math.min(Math.min(l,o),n));
+        int b_freq=0;
+        int a_freq=0;
+        int l_freq=0;
+        int o_freq=0;
+        int n_freq=0;
+        for(char ch:text.toCharArray()) {
+            if(ch=='b') b_freq++;
+            else if(ch=='a')a_freq++;
+            else if(ch=='l')l_freq++;
+            else if(ch=='o')o_freq++;
+            else if(ch=='n')n_freq++;
+        }
+        return Math.min(Math.min(b_freq, a_freq), Math.min(n_freq, Math.min(l_freq/2, o_freq/2)));
     }
 }
