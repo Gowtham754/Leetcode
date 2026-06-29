88d1c3988fcc710fdc20189297ead8670e98355a
GOWTHAM N <nakkagowtham399@gmail.com>
2026-06-29 15:51:05 +0530

    Time: 12 ms (5.42%) | Memory: 45.3 MB (10.44%) - LeetSync

---
+++ 90-subsets-ii/subsets-ii.java
@@ -0,0 +1,22 @@
+class Solution {
+    void f(int idx,int []nums,ArrayList<Integer>temp,ArrayList<List<Integer>>res){
+        int n=nums.length;
+        if(idx>=n){
+             if(!res.contains(new ArrayList<>(temp))){
+            res.add(new ArrayList<>(temp));
+             }
+            return;
+        }
+       
+        temp.add(nums[idx]);
+        f(idx+1,nums,temp,res);
+        temp.remove(temp.size()-1);
+        f(idx+1,nums,temp,res);
+    }
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+         ArrayList<List<Integer>>res=new ArrayList<>();
+       f(0,nums,new ArrayList<Integer>(),res);
+       return res;
+    }
+}
