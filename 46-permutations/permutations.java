265d4f63e1983e44f948f4dedf24c6060fef1ba3
GOWTHAM N <nakkagowtham399@gmail.com>
2026-06-30 15:46:22 +0530

    Time: 1 ms (87.30%) | Memory: 45.2 MB (95.29%) - LeetSync

---
+++ 46-permutations/permutations.java
@@ -0,0 +1,25 @@
+class Solution {
+    public static void f(int nums[],int idx,List<List<Integer>>res,ArrayList<Integer>temp,boolean arr[]){
+       if(temp.size()==nums.length){
+        res.add(new ArrayList<>(temp));
+       }
+       
+       for(int i=0;i<nums.length;i++){
+          if(arr[i]) continue;
+            temp.add(nums[i]);
+            arr[i]=true;
+          
+        f(nums,0,res,temp,arr);
+          arr[i]=false;
+          temp.remove(temp.size()-1);
+
+       }
+         
+    }
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>>res=new ArrayList<>();
+        boolean arr[]=new boolean[nums.length];
+        f(nums,0,res,new ArrayList<Integer>(),arr);
+        return res;
+    }
+}
