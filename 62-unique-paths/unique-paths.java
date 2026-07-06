9a00bacb92e189ddd15174886be849e7405f9876
GOWTHAM N <nakkagowtham399@gmail.com>
2026-07-06 12:42:41 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (65.50%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,19 +1,23 @@
 class Solution {
    
     public int uniquePaths(int m, int n) {
-        int dp[][]=new int[m][n];
+        int row1[]=new int[n];
+        int row2[]=new int[n];
         for(int i=0;i<n;i++){
-            dp[m-1][i]=1;
+            row2[i]=1;
         }
-         for(int i=0;i<m;i++){
-            dp[i][n-1]=1;
-        }
+        row1[n-1]=1;
+        
         for(int i=m-2;i>=0;i--){
             for(int j=n-2;j>=0;j--){
-                dp[i][j]=dp[i][j+1]+dp[i+1][j];
+                row1[j]=row1[j+1]+row2[j];
+                
             }
+            int[] temp=row1;
+                row1=row2;
+                row2=temp;
         }
-        return dp[0][0];
+        return row2[0];
        
     }
 }
