2299a16cdff10b329c9a63cbaeb37ac280e736a7
GOWTHAM N <nakkagowtham399@gmail.com>
2026-08-27 06:50:15 +0530

    Time: 1 ms (98.29%) | Memory: 42.9 MB (97.07%) - LeetSync

--- 3379-score-of-a-string/score-of-a-string.java
+++ 3379-score-of-a-string/score-of-a-string.java
@@ -1,13 +1,9 @@
 class Solution {
     public int scoreOfString(String s) {
-        int res=0;
-        ArrayList<Integer>list=new ArrayList<>();
-        for(char ch:s.toCharArray()){
-            list.add((int)(ch));
-        }
-        for(int i=1;i<list.size();i++){
-            res+=Math.abs(list.get(i-1)-list.get(i));
-        }
-        return res;
+        int sum=0;
+       for(int i=0;i<s.length()-1;i++){
+         sum+=Math.abs(s.charAt(i)-s.charAt(i+1));
+       }  
+       return sum;
     }
 }
