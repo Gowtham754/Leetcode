e917b69431528a72c6e0505309f47318038b08d2
GOWTHAM N <nakkagowtham399@gmail.com>
2026-08-28 06:38:53 +0530

    Time: 3 ms (66.24%) | Memory: 43.7 MB (53.96%) - LeetSync

---
+++ 1666-make-the-string-great/make-the-string-great.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String makeGood(String s) {
+       Stack<Character>st=new Stack<>();
+       for(char ch:s.toCharArray()){
+        if(!st.isEmpty()){
+            char c=st.peek();
+            if(Math.abs(c-ch)==32){
+                st.pop();
+            }
+            else{
+                st.push(ch);
+            }
+        }
+        else{
+            st.push(ch);
+        }
+       }
+       StringBuilder sb=new StringBuilder();
+       while(!st.isEmpty()){
+        sb.insert(0,st.pop());
+       } 
+       return sb.toString();
+    }
+}
