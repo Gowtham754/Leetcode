4bb771ba1b6b4fa3e7144ec02fa0a728b975d753
GOWTHAM N <nakkagowtham399@gmail.com>
2026-09-08 06:53:37 +0530

    Time: 1 ms (83.65%) | Memory: 42.7 MB (22.48%) - LeetSync

--- 4245-count-commas-in-range/count-commas-in-range.java
+++ 4245-count-commas-in-range/count-commas-in-range.java
@@ -1,23 +1,9 @@
 class Solution {
-    public int len(int n){
-        int cnt=0;
-        while(n>0){
-            cnt++;
-            n/=10;
-        }
-        return cnt;
-    }
-    public int countCommas(int n) {
-        int cnt=0;
-        if(len(n)<4){
-            return 0;
-        }
-        else{
-         while(len(n)>=4){
-            cnt++;
-            n--;
-         }
-        }
-        return cnt;
-    }
+    
+public int countCommas(int n) {
+   if(n<1000){
+    return 0;
+   }
+   return(n-1000)+1;
+}
 }
