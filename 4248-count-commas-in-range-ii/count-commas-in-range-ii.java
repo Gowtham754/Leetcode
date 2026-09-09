6b74f322f7bca7a946fe09afd4c90a1da6583d3d
GOWTHAM N <nakkagowtham399@gmail.com>
2026-09-09 07:02:39 +0530

    Time: 1 ms (99.36%) | Memory: 42.7 MB (56.41%) - LeetSync

---
+++ 4248-count-commas-in-range-ii/count-commas-in-range-ii.java
@@ -0,0 +1,21 @@
+class Solution{
+    public long countCommas(long n){
+        long count=0;
+        long start=1000;
+        long commas=1;
+
+        while(start<=n){
+            long end=start*1000-1;
+
+            if(n<end){
+                end=n;
+            }
+
+            count+=(end-start+1)*commas;
+            start*=1000;
+            commas++;
+        }
+
+        return count;
+    }
+}
